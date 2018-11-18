2e499497a118bbd252fbaa7cb5f2d82d807d3b56
Eric Lin <ttttt123465@gmail.com>
2018-11-18 23:30:45 +0800

    refactor Authorize Component

--- spotify-chrome-extension/src/components/Authorize/Authorize.js
+++ spotify-chrome-extension/src/components/Authorize/Authorize.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import { page } from '../../helper/constant';
-import { setPage } from '../../helper/util';
-import { config } from '../../server/config';
+import { setPage, sendRefreshRequest } from '../../helper/util';
 
 class Authorize extends Component {
     constructor(props) {
         super(props);
         this.state = { token: "" };
         this.setStateToken = this.setStateToken.bind(this);
-        this.onSetRefreshToken = this.onSetRefreshToken.bind(this);
+        this.onSendRefreshRequest = this.onSendRefreshRequest.bind(this);
+        this.handleApiRequest=this.handleApiRequest.bind(this);
         this.onBackToLogin = this.onBackToLogin.bind(this);
     }
     setStateToken(event) {
         this.setState({ token: event.target.value });
     }
-    onSetRefreshToken() {
-        let data = new URLSearchParams();
-        data.append('refresh_token', this.state.token);
-
-        window.fetch('http://localhost:3001/refreshtoken', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: data,
-            mode: 'cors'
-        }).then(response => response.json())
-            .then(j => console.log(j))
+    onSendRefreshRequest() {
+        sendRefreshRequest(this.state.token)
+            .then(response => response.json())
+            .then(this.handleApiRequest)
             .catch(function (err) {
                 console.log('err: ', err);
             });
-        this.props.onSetRefreshToken(this.state.token);
     }
+    handleApiRequest(data){
+        if (data.access_token) {
+            this.props.onSetRefreshToken(this.state.token);
+        }
+        else {
+            console.log(data);
+        }
+    }
+
     onBackToLogin() {
         setPage(page.LOGIN);
         this.props.handler(page.LOGIN);
@@ -41,7 +40,7 @@ class Authorize extends Component {
             <div>
                 <h2>refresh_token</h2>
                 <input type="text" onChange={this.setStateToken} />
-                <button onClick={this.onSetRefreshToken}>Submit</button>
+                <button onClick={this.onSendRefreshRequest}>Submit</button>
                 <div>
                     <button onClick={this.onBackToLogin}>back to login</button>
                 </div>
--- spotify-chrome-extension/src/helper/util.js
+++ spotify-chrome-extension/src/helper/util.js
@@ -2,6 +2,20 @@ export const stringify = (params) => {
     return Object.keys(params).map(key => key + "=" + encodeURIComponent(params[key])).join('&');
 }
 
-export const setPage=(page)=>{
+export const setPage = (page) => {
     localStorage.setItem("page", page);
+}
+
+export const sendRefreshRequest = (token) => {
+    const data = new URLSearchParams();
+    data.append('refresh_token', token);
+
+    return window.fetch('http://localhost:3001/refreshtoken', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: data,
+        mode: 'cors'
+    });
 }
