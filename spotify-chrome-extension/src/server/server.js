c75b12239b4a76a95224d7da00a2d61aa50e782a
Eric Lin <ttttt123465@gmail.com>
2018-11-18 23:02:50 +0800

    refactor server

--- spotify-chrome-extension/src/server/server.js
+++ spotify-chrome-extension/src/server/server.js
@@ -1,11 +1,8 @@
 import express from 'express';
-import { config } from './config';
-
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
-import { sendAuthReq } from './util';
-import axios from 'axios';
+import { sendAuthReq, sendRefreshReq } from './util';
 
 const app = express();
 const port = 3001;
@@ -21,21 +18,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/refreshtoken', (req, res) => {
-    console.log(req.body);
-    const apiRequest = {
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        params: {
-            grant_type: 'refresh_token',
-            refresh_token: req.body.refresh_token
-        },
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + (new Buffer(config.client_id + ':' + config.client_secret).toString('base64'))
-        }
-    };
-    axios(apiRequest).then(response => {
-        res.json(response.data);
-    }).catch(err => res.json(err.response.data));
+    sendRefreshReq(req.body.refresh_token)
+        .then(response => res.json(response.data))
+        .catch(err => res.json(err.response.data));
 })
+
 app.listen(port, () => console.log(`server is listening on port ${port}`))
--- spotify-chrome-extension/src/server/util.js
+++ spotify-chrome-extension/src/server/util.js
@@ -1,18 +1,36 @@
 import { config } from './config';
 import axios from 'axios';
+
 export const sendAuthReq = (code) => {
-    const apiRequest = {
+    const params = {
+        code: code,
+        grant_type: 'authorization_code',
+        redirect_uri: config.redirect_uri
+    };
+    return sendApiRequest(params);
+};
+
+export const sendRefreshReq = (refresh_token) => {
+    const params = {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+    };
+    return sendApiRequest(params);
+}
+
+export const sendApiRequest=(params)=>{
+    const request = getApiRequest(params);
+    return axios(request);
+}
+
+const getApiRequest = (params) => {
+    return {
         method: 'post',
         url: 'https://accounts.spotify.com/api/token',
-        params: {
-            code: code,
-            grant_type: 'authorization_code',
-            redirect_uri: config.redirect_uri
-        },
+        params: params,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Basic ' + (new Buffer(config.client_id + ':' + config.client_secret).toString('base64'))
         }
     };
-    return axios(apiRequest);
-};
+}
