ff8870c671681477470368b68c478d6b91de96b6
Eric Lin <ttttt123465@gmail.com>
2018-11-18 22:46:22 +0800

    finish refresh access token

--- spotify-chrome-extension/src/components/Authorize/Authorize.js
+++ spotify-chrome-extension/src/components/Authorize/Authorize.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import { page } from '../../helper/constant';
 import { setPage } from '../../helper/util';
+import { config } from '../../server/config';
 
 class Authorize extends Component {
     constructor(props) {
         super(props);
         this.state = { token: "" };
         this.setStateToken = this.setStateToken.bind(this);
-        this.OnSetRefreshToken = this.onSetRefreshToken.bind(this);
-        this.onBackToLogin=this.onBackToLogin.bind(this);
+        this.onSetRefreshToken = this.onSetRefreshToken.bind(this);
+        this.onBackToLogin = this.onBackToLogin.bind(this);
     }
     setStateToken(event) {
         this.setState({ token: event.target.value });
     }
     onSetRefreshToken() {
+        let data = new URLSearchParams();
+        data.append('refresh_token', this.state.token);
+
+        window.fetch('http://localhost:3001/refreshtoken', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: data,
+            mode: 'cors'
+        }).then(response => response.json())
+            .then(j => console.log(j))
+            .catch(function (err) {
+                console.log('err: ', err);
+            });
         this.props.onSetRefreshToken(this.state.token);
     }
     onBackToLogin() {
--- spotify-chrome-extension/src/server/server.js
+++ spotify-chrome-extension/src/server/server.js
@@ -1,15 +1,41 @@
 import express from 'express';
+import { config } from './config';
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import helmet from 'helmet';
 import { sendAuthReq } from './util';
+import axios from 'axios';
 
 const app = express();
 const port = 3001;
 
 app.use(helmet());
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     sendAuthReq(req.query.code)
-        .then(response=>res.json(response.data))
-        .catch(err=>res.json(err.response.data));
+        .then(response => res.json(response.data))
+        .catch(err => res.json(err.response.data));
+})
+
+app.post('/refreshtoken', (req, res) => {
+    console.log(req.body);
+    const apiRequest = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        params: {
+            grant_type: 'refresh_token',
+            refresh_token: req.body.refresh_token
+        },
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + (new Buffer(config.client_id + ':' + config.client_secret).toString('base64'))
+        }
+    };
+    axios(apiRequest).then(response => {
+        res.json(response.data);
+    }).catch(err => res.json(err.response.data));
 })
 app.listen(port, () => console.log(`server is listening on port ${port}`))
